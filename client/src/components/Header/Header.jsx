54dd8c020453704c10f75b2285b5b07bdebc3457
its-akshay <keswani014@gmail.com>
2023-03-14 18:17:06 +0530

    changes for materialUI

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,6 +8,24 @@ import Search from "./Search/Search";
 import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
 
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Redirect, Route } from 'react-router';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import Login from '../Login/Login';
+import Register from '../Login/Register';
+import Dashboard from '../Login/Dashboard';
+import '../Login/Login.css';
+
+import { Routes } from "react-router";
+// import { Route } from "react-router";
+// import react, { Component } from 'react';
+// import swal from 'sweetalert';
+// import { Button, TextField, link } from '@material-ui/core';
+
 const Header = () => {
     const [scrolled, setScrolled] = useState(false);
     const [searchModal, setSearchModal] = useState(false);
@@ -41,16 +59,25 @@ const Header = () => {
                     <div className="center" onClick={() => navigate("/")}>
                         Artisans
                     </div>
-                    
+
                     <div className="right">
                         <TbSearch onClick={() => setSearchModal(true)} />
-                        <AiOutlineHeart />
+                        {/* <AiOutlineHeart /> */}
+                        <BrowserRouter>
+                            <Routes>
+                                <Route exact path='/' component={Login} />
+                                <Route exact path='/register' component={Register} />
+                                <Route path='/dashboard' component={Dashboard} />
+                                {/* <Route component={NotFound}/> */}
+                            </Routes>
+                        </BrowserRouter>,
                         <span
                             className="cart-icon"
                             onClick={() => setShowCart(true)}
                         >
                             <CgShoppingCart />
                             {!!cartCount && <span>{cartCount}</span>}
+
                         </span>
                     </div>
                 </div>
